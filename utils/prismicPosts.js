823e2e28bc83027df5c1e8ab6e3ce84e08d6318d
Josh Marsh <josh.marsh@designory.com>
2021-02-24 14:17:38 -0600

    fixed typos

--- utils/prismicPosts.js
+++ utils/prismicPosts.js
@@ -1,6 +1,6 @@
 import Prismic from 'prismic-javascript'
 
-const REPOSITORY = `joshmarsh1`
+const REPOSITORY = process.env.NEXT_PUBLIC_PRISMIC_REPO_NAME
 export const API_URL = `https://${REPOSITORY}.cdn.prismic.io/api/v2`
 export const API_TOKEN = process.env.NEXT_PUBLIC_PRISMIC_ACCESS_TOKEN
 
