77e286dcaa6622e34e8bad617df46f06f87b3b4f
Josh Marsh <josh.marsh@designory.com>
2021-02-24 14:27:06 -0600

    added posts param to home

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import Footer from '@components/Footer'
 
 import Post from '@components/Post'
 
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <div className="container">
       <Head>
@@ -29,6 +29,40 @@ export default function Home() {
       </main>
 
       <Footer />
+
+      <style jsx>{`
+        .container {
+          height: 100vh;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+        .posts {
+          display: flex;
+        }
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu,
+            Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+        }
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
     </div>
   )
 }
